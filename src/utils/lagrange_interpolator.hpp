d065e7dab56753266ae77c1040c72f89585ba74c
Alireza <rashti.alireza@gmail.com>
2024-02-13 17:46:38 -0500

    More linting.

--- src/utils/lagrange_interpolator.hpp
+++ src/utils/lagrange_interpolator.hpp
@@ -5,31 +5,35 @@
 //========================================================================================
 //! \file lagrange_interpolator.hpp
 
-#include <iostream>
-#include <cmath>
-#include <list>
-#include "coordinates/cell_locations.hpp"
-#include "mesh/mesh.hpp"
 #include "athena.hpp"
 #include "athena_tensor.hpp"
+#include "coordinates/cell_locations.hpp"
+#include "mesh/mesh.hpp"
+#include <cmath>
+#include <iostream>
+#include <list>
 
 class LagrangeInterpolator {
- public:
-  LagrangeInterpolator(MeshBlockPack *pmy_pack, Real rcoords[3]);
-  ~LagrangeInterpolator() = default;
+  public:
+    LagrangeInterpolator(MeshBlockPack *pmy_pack, Real rcoords[3]);
+    ~LagrangeInterpolator() = default;
 
-  void SetInterpolationIndices();
-  void CalculateWeight();
-  Real Interpolate(DvceArray5D<Real> &val, int nvars);
-  Real InterpolateTensor(AthenaTensor<Real,TensorSymm::NONE,3,1> &val,int nvars);
-  Real ResetPointAndInterpolate(DvceArray5D<Real> &val, int nvars, Real rcoords2[3]);
-  bool point_exist; // point exist on this rank (meshblock pack)
- private:
-  HostArray1D<Real> rcoord; // xyz coordinate for interpolated value
+    void SetInterpolationIndices();
+    void CalculateWeight();
+    Real Interpolate(DvceArray5D<Real> &val, int nvars);
+    Real InterpolateTensor(
+      AthenaTensor<Real, TensorSymm::NONE, 3, 1> &val, int nvars);
+    Real ResetPointAndInterpolate(
+      DvceArray5D<Real> &val, int nvars, Real rcoords2[3]);
+    bool point_exist; // point exist on this rank (meshblock pack)
+  private:
+    HostArray1D<Real> rcoord; // xyz coordinate for interpolated value
 
-  int nvars; // index of the variable for interpolation
-  MeshBlockPack* pmy_pack;  // ptr to MeshBlockPack containing this Hydro
-  int ng; // number of ghost cell, which sets interpolation level automatically
-  HostArray1D<int> interp_indcs;   // indices of MeshBlock and zones therein for interp
-  HostArray2D<Real> interp_wghts;  // weights for interpolation
+    int nvars;               // index of the variable for interpolation
+    MeshBlockPack *pmy_pack; // ptr to MeshBlockPack containing this Hydro
+    int
+      ng; // number of ghost cell, which sets interpolation level automatically
+    HostArray1D<int>
+      interp_indcs; // indices of MeshBlock and zones therein for interp
+    HostArray2D<Real> interp_wghts; // weights for interpolation
 };
